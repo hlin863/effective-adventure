a5d63cfe0da94fb36ab8b3f14c891c9095b77b91
hlin863 <55798830+hlin863@users.noreply.github.com>
2022-06-14 14:11:17 +0100

    Update sign_up.js
    Attempt to store the user as JSON structure in a JSON file.

--- JS/sign_up.js
+++ JS/sign_up.js
@@ -17,6 +17,23 @@ document.getElementById('sign-up').addEventListener("click", function() {
 
     // let userFile = require("File/user.json");
 
-    const userFile = require("./user.json");
+    const userFile = require(["./user.json"], (json) => { console.log(json); });
+
+    const userEntry = {
+        "first_name": user.getFirstName(),
+        "last_name": user.getLastName(),
+        "email": user.getEmail(),
+        "password": user.getPassword()
+    }
+
+    const userJSON = JSON.stringify(userEntry);
+
+    userFile.writeFile("./user.json", userJSON, function(err) {
+        if (err){
+            console.log(err);
+        } else {
+            console.log("User created");
+        }
+    });
 
 })
