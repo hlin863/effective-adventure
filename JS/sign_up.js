512f0888a795da6d1e9a45d6b8f8fed94a1e2801
hlin863 <55798830+hlin863@users.noreply.github.com>
2022-06-14 11:22:50 +0100

    Fixed display register detail bug

--- JS/sign_up.js
+++ JS/sign_up.js
@@ -1,4 +1,4 @@
-document.getElementById("sign_up").addEventListener("click", function() {
+document.getElementById('sign-up').addEventListener("click", function() {
 
     var first_name = document.getElementById("first-name").value;
     var last_name = document.getElementById("last-name").value;
